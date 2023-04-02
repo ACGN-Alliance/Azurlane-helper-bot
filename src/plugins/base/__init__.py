8b0e31f18d063b17c440cdf765a6426c22e00577
MRSlou <79364553+MRSlouzk@users.noreply.github.com>
2023-04-02 15:51:38 +0800

    Revert "修bug & 更新README"
    This reverts commit 86328a0b375042ef5391e70657bb1467e8e0ffc8.

---
+++ src/plugins/base/__init__.py
@@ -0,0 +1,4 @@
+from .admin_command import *
+from .func_helper import *
+from .group_admin_command import *
+from .sync import *
--- src/plugins/base/sync.py
+++ src/plugins/base/sync.py
@@ -3,14 +3,13 @@ from nonebot.log import logger
 from nonebot import get_driver
 
 from src.plugins.exception import RemoteFileNotExistsException
-from src.plugins.config import config
 
 driver = get_driver()
 
 class GithubHook(object):
-    if(config.download_source == "github"):
+    if(driver.config.download_source == "github"):
         url_prefix = 'https://api.github.com/repos/MRSlouzk/nonebot-plugin-azurlane-assistant-data/'
-    elif(config.download_source == "gitee"):
+    elif(driver.config.download_source == "gitee"):
         url_prefix = 'https://gitee.com/mrslouzk/nonebot-plugin-azurlane-assistant-data/'
     else:
         url_prefix = 'https://api.github.com/repos/MRSlouzk/nonebot-plugin-azurlane-assistant-data/'
@@ -41,25 +40,24 @@ class GithubHook(object):
         for path in sync_list:
             url = self.url_prefix + 'contents/' + path
             try:
-                if(config.PROXY): httpx.get(url, proxies=config.PROXY)
+                if(driver.config.PROXY): httpx.get(url, proxies=driver.config.PROXY)
                 else: r = httpx.get(url)
-            except Exception:
+            except Exception as e:
                 raise Exception("Github API请求失败, 请检查代理设置")
             if(r.status_code == 404): raise RemoteFileNotExistsException(path)
             elif(r.status_code != 200): raise Exception("Github API出错")
-            logger.info(r.status_code)
             content = base64.b64decode(r.json()['content'])
             open(self.root_path + _path + path, "w", encoding="utf-8").write(content.decode("utf-8"))
 
 async def data_sync():
     is_need_inited = False
-    data_path_lst = ["data/azurlane", "data/word_bank", "data/bili"]
+    data_path_lst = ["data/azurlane/", "data/word_bank"]
     for path in data_path_lst:
         if not os.path.exists(path):
             os.makedirs(path)
             is_need_inited = True
 
-    data_file = ["data/group.json", "data/user.json", "data/group_cmd.json", "data/group_func.json", "data/config.json", "data/bili/latest.json"]
+    data_file = ["data/group.json", "data/user.json", "data/group_cmd.json", "data/group_func.json", "data/bili/latest.json"]
     for file in data_file:
         if not os.path.exists(file):
             with open(file, "w", encoding="utf-8") as f:
@@ -71,7 +69,7 @@ async def data_sync():
         with open("data/sync.json", "w", encoding="utf-8") as f:
             f.write(json.dumps(data))
     except Exception as e:
-        logger.error(str(e) + "\n获取同步列表失败, 已终止数据同步")
+        logger.error(e + "\n获取同步列表失败, 已终止数据同步")
         return False
 
     if is_need_inited:
