2856e6607d7131013d9ed39825925605faab91aa
MRSlouzk <MRSlouu@163.com>
2023-06-03 16:29:52 +0800

    fix: 小修复

--- src/plugins/server_status/__init__.py
+++ src/plugins/server_status/__init__.py
@@ -17,19 +17,3 @@ __usage__ = """服务器状态监测
 __version__ = "0.0.1"
 
 from .if_on import *
-from nonebot import logger
-from nonebot_plugin_apscheduler import scheduler
-import nonebot
-
-auto_check = cfg["func"]["server_status_monitor_refresh_time"]
-if(not isinstance(auto_check, int)):
-    pass
-else:
-    if auto_check < 30 or auto_check > 6000:
-        logger.info(f"server_status_monitor_refresh_time{auto_check}, 正常范围为1~1440, 定时检查将不会生效")
-    else:
-        @scheduler.scheduled_job("interval", seconds=auto_check)
-        async def check():
-            (bot,) = nonebot.get_bots().values()
-            await push_msg(bot)
-            logger.info(f"[自动检查]已检查服务器状态")
